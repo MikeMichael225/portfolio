b1859e0ee2a5d552fbdb7d33f68aabaa8ea75c8f
MikeMichael225 <michal290704@gmail.com>
2020-11-01 12:17:30 +0100

    first commit

--- resources/scripts/json/projects_data.js
+++ resources/scripts/json/projects_data.js
@@ -3,41 +3,41 @@ const projects = `
     {
         "name": "Comic Book Reader",
         "description": "This project is an application for reading comic books. It was made with HTML, CSS and JS. How does it work? You just upload a zip file containing a comic book you want, then javascript unpacks the archive and displays your comic! There is also an UI for jumping between pages.",
-        "href": "./cbr-reader/index.html"
+        "href": "https://mikemichael225.github.io/comic_book_reader/"
     },
     {
         "name": "Todo List",
         "description": "As the name says it is a simple todo list app. It was made with HTML, CSS and JS. The app allows to add and remove todos and also stores your todos as a cookie, so you won't lose them even when you restart your browser.",
-        "href": "./todo/index.html"
+        "href": "https://mikemichael225.github.io/todo/"
     },
     {
         "name": "Simple Text Editor",
         "description": "This app as the name suggests is a simple text editor. It was made with HTML, CSS and JS. It features file saving and file editing. To edit a file you just drag and drop the file anywhere on the page.",
-        "href": "./simple-text-editor/index.html"
+        "href": "https://mikemichael225.github.io/simple_text_editor/"
     },
     {
         "name": "Black & White Videos",
         "description": "This webside allows you to play any video you want in black and white. To play a video just simply drag and drop one. The video is displayed on HTML Canvas. I recomment you don't use videos with resolution higher than 640x320 as the performance may not be great.",
-        "href": "./BWvideo/index.html"
+        "href":"https://mikemichael225.github.io/black_white_video_converter/"
     },
     {
         "name": "Tic Tac Toe",
         "description": "Like the name says it's a Tic Tac Toe game made with HTML, CSS and JS. It features SFX.",
-        "href": "./tictactoe/index.html"
+        "href": "https://mikemichael225.github.io/tic_tac_toe/"
     },
     {
         "name": "Profile Cards",
         "description": "This webside displays profile cards of random (fake) people. Random identities are fetched from a API. <br> <br> API Used: <a target='_blank' href='https://randomuser.me/'>https://randomuser.me/api</a>",
-        "href": "./css-cards/index.html"
+        "href": "https://mikemichael225.github.io/profile_cards/"
     },
     {
         "name": "Password Generator",
         "description": "This website allows you to generate random passwords combined from uppercase and lowercase letters, numbers and special characters. You can customize which characters should your password include.",
-        "href": "./passwordGenerator/index.html"
+        "href": "https://mikemichael225.github.io/password_generator/"
     },
     {
         "name": "Analogue Clock",
         "description": "This website shows current time on analogue clock created with HTML, CSS and JS.",
-        "href": "./clock/index.html"
+        "href": "https://mikemichael225.github.io/analogue_clock/"
     }
 ]`;
